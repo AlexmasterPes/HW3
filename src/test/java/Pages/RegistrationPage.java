308840c0a4a795a0c638e52a95fecdb667769179
APesegov <metallurgkras@mail.ru>
2026-01-19 01:22:12 +0700

    PO 1 try

---
+++ src/test/java/Pages/RegistrationPage.java
@@ -0,0 +1,153 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationPage {
+
+    private final SelenideElement firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            genderWrapper = $("#genterWrapper"),
+            userNumberInput = $("#userNumber"),
+            calendarInput = $("#dateOfBirthInput"),
+            subjectsInput = $("#subjectsInput"),
+            hobbiesWrapper = $("#hobbiesWrapper"),
+            uploadPicture = $("#uploadPicture"),
+            currentAddressInput = $("#currentAddress"),
+            stateDropdown = $("#state"),
+            stateCityWrapper = $("#stateCity-wrapper"),
+            cityDropdown = $("#city"),
+            submit = $("#submit"),
+            modalDialog = $(".modal-dialog"),
+            closeModal = $("#closeLargeModal");
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+    pages.components.TableComponent tableResponsiveComponent = new pages.components.TableComponent();
+
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        return this;
+    }
+
+    public RegistrationPage removeBanners() {
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setEmail(String value) {
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setGender(String value) {
+        genderWrapper.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setUserNumber(String value) {
+        userNumberInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setDateOfBirth(String day, String month, String year) {
+        calendarInput.click();
+        calendarComponent.setDate(day, month, year);
+
+        return this;
+    }
+
+    public RegistrationPage setSubjects(String value) {
+        subjectsInput.setValue(value).pressEnter();
+
+        return this;
+    }
+
+    public RegistrationPage setHobbies(String value) {
+        hobbiesWrapper.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setUploadPicture(String filePath) {
+        uploadPicture.uploadFromClasspath(filePath);
+
+        return this;
+    }
+
+    public RegistrationPage setCurrentAddress(String value) {
+        currentAddressInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setStateDropdown() {
+        stateDropdown.click();
+
+        return this;
+    }
+
+    public RegistrationPage setStateCityWrapper(String value) {
+        stateCityWrapper.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setCityDropdown() {
+        cityDropdown.click();
+
+        return this;
+    }
+
+    public RegistrationPage setSubmit() {
+        submit.click();
+
+        return this;
+    }
+
+    public RegistrationPage setModalDialog() {
+        modalDialog.should(appear);
+
+        return this;
+    }
+
+    public RegistrationPage checkResult(String key, String value) {
+        tableResponsiveComponent.setTable(key, value);
+
+        return this;
+    }
+
+    public RegistrationPage closeModal() {
+        closeModal.click();
+
+        return this;
+    }
+
+    public RegistrationPage getModalDialog() {
+        modalDialog.shouldNotBe(visible);
+
+        return this;
+    }
+}
---
+++ src/test/java/tests/HomeWorkPO.java
@@ -0,0 +1,75 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+public class HomeWorkPO extends tests.TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    void fillFormTest() {
+        registrationPage.openPage()
+                .removeBanners()
+                .setFirstName("Alexander")
+                .setLastName("Pesegov")
+                .setEmail("metallurgkras@mail.ru")
+                .setGender("Male")
+                .setUserNumber("9632614307")
+                .setDateOfBirth("15", "October", "1994")
+                .setSubjects("Economics")
+                .setHobbies("Sports")
+                .setUploadPicture("таблица.jpg")
+                .setCurrentAddress("Красноярск, Урицкого 31")
+                .setStateDropdown()
+                .setStateCityWrapper("NCR")
+                .setCityDropdown()
+                .setStateCityWrapper("Delhi")
+                .setSubmit()
+                .setModalDialog()
+                .checkResult ("Student Name", "Alexander Pesegov")
+                .checkResult ("Student Email", "metallurgkras@mail.ru")
+                .checkResult ("Gender", "Male")
+                .checkResult ("Mobile", "9632614307")
+                .checkResult ("Date of Birth", "15 October,1994")
+                .checkResult ("Subjects", "Economics")
+                .checkResult ("Hobbies", "Sports")
+                .checkResult ("Picture", "таблица.jpg")
+                .checkResult ("Address", "Красноярск, Урицкого 31")
+                .checkResult ("State and City", "NCR Delhi")
+                .checkResult ("Picture", "таблица.jpg")
+                .closeModal();
+    }
+
+    @Test
+    void minRequired() {
+        registrationPage.openPage()
+                .removeBanners()
+                .setFirstName("Alexander")
+                .setLastName("Pesegov")
+                .setGender("Male")
+                .setUserNumber("9632614307")
+                .setDateOfBirth("15", "October", "1994")
+                .setSubmit()
+                .setModalDialog()
+                .checkResult ("Student Name", "Alexander Pesegov")
+                .checkResult ("Gender", "Male")
+                .checkResult ("Mobile", "9632614307")
+                .checkResult ("Date of Birth", "15 October,1994")
+                .closeModal();
+    }
+
+    @Test
+    void Negative() {
+        registrationPage.openPage()
+                .removeBanners()
+                // поле LastName пустое
+                .setFirstName("Alexander")
+                .setGender("Male")
+                .setUserNumber("9632614307")
+                .setDateOfBirth("15", "October", "1994")
+                .setSubmit()
+                .getModalDialog();
+
+    }
+}
